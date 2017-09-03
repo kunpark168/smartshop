4f5e3e4f5f16dd6ba62ddf8af34c51ba0f893dad
KunPC\KunPark <pikakun19@gmail.com>
2017-09-03 21:12:15 +0700

    KunPark : Add Project

---
+++ app/src/main/java/master/android/nt/smartshop/MainActivity.java
@@ -0,0 +1,13 @@
+package master.android.nt.smartshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
